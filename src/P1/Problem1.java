1cb1d3026b2b2bc1286c2f1ccbdb10a16373b497
ellethykhaled-github <ellethykhaled@yahoo.com>
2023-09-01 00:57:14 +0300

    Database Connection Manager initialization

---
+++ src/P1/Problem1.java
@@ -0,0 +1,19 @@
+package P1;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Problem1 {
+
+    public static void main(String... args) {
+        DatabaseConnectionManager databaseConnectionManager = new DatabaseConnectionManager(
+                "localhost", "Company", "postgres", "password");
+
+        try {
+            Connection connection = databaseConnectionManager.getConnection();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
